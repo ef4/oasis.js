1de4a8bb0e436b93c6c743d3b48c697237712eee
Edward Faulkner <ef@alum.mit.edu>
2015-09-03 17:10:12 -0400

    clearing out old Grunt infrastructure

--- configurations/symlink.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  samples: {
-    files: [{
-      expand: true,
-      src: ['samples/'],
-      dest: 'tmp/public/'
-    }]
-  }
-};
--- configurations/uglify.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  browser: {
-    src: ['dist/oasis.js'],
-    dest: 'dist/oasis.min.js'
-  }
-};
